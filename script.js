434782690b5ee2c73f8a26e40ae46300c1573af8
AlmanMans077 <158796057+AlmanMans077@users.noreply.github.com>
2024-03-05 06:13:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn')
+const spinner = document.getElementById('spinner')
+
+button.addEventListener('click', () => {  spinner.classList.toggle('complete')
+})
